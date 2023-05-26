54101e64cd47461c29850204cf73c9b18ecef69c
ibrahimcanerdogan <i.erdogan@ebebek.com>
2023-05-27 00:25:59 +0300

    Default set bottom navigation view.

--- app/src/main/java/com/ibrahimcanerdogan/valorantguideapp/MainActivity.kt
+++ app/src/main/java/com/ibrahimcanerdogan/valorantguideapp/MainActivity.kt
@@ -23,7 +23,7 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         replaceFragment(AgentFragment())
-
+        binding.bottomNavigationView.menu.getItem(2).isChecked = true
         binding.bottomNavigationView.setOnItemSelectedListener {
             when(it.itemId) {
                 R.id.navigation_agent -> replaceFragment(AgentFragment())
